f03daa0c0981ca6d445643ef7ddf83f9c8bec72d
andrew-ware <andrew.ware@gordon.edu>
2016-11-30 15:04:54 -0500

    Basic working version

--- weather/index.js
+++ weather/index.js
@@ -8,7 +8,7 @@ function getWeatherData(url, cb) {
   $.ajax({
     url: url,
     dataType: "jsonp",
-    success: function (data) {
+    success: function(data) {
       cb(data);
     }
   });
@@ -17,5 +17,51 @@ function getWeatherData(url, cb) {
 function handleData(data) {
   // HANDLE WEATHER DATA
   console.log(data);
-  document.getElementById('root').innerHTML = JSON.stringify(data, null, 2);
+  // document.getElementById('root').innerHTML = JSON.stringify(data, null, 2);
+
+  var customHTML;
+
+  var currently = data.currently;
+  var currentlyHTML = '<div id="currently">' + 
+    '<h1>Currently</h1>' + 
+    '<ul>' +
+    '<li>Temperature: ' + currently.temperature + '</li>' +
+    '<li>Humidity: ' + currently.humidity + '</li>' +
+    '<li>Wind Speed: ' + currently.windSpeed + '</li>' +
+    '</ul>' +
+    '</div>';
+
+  var daily = data.daily.data;
+  var today = (new Date()).getDay();
+  var dailyHTML = '<div id="daily">' +
+    '<h1>Daily Forecast</h1>' +
+    daily.map(function(day, i) {
+      if (i === 0) return; // ignore today
+      return '<div class=day>' +
+        '<h5>' + getDayFromNow(today, i) + '</h5>' +
+        '<ul>' +
+        '<li>Min: ' + day.temperatureMin + '</li>' +
+        '<li>Max: ' + day.temperatureMax + '</li>' +
+        '</ul>' +
+        '</div>';
+    }).join('') +
+    '</div>';
+
+  var customHTML = currentlyHTML + dailyHTML;
+
+  document.getElementById('root').innerHTML = customHTML;
+}
+
+function getDayFromNow(today, i) {
+  var days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  return days[(today + i) % 7];
 }
